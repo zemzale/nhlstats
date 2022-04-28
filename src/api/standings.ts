b57d0f4704695af0d7f894cf614410d644f81758
Alberts Zemzale <alberts.zemzale@gmail.com>
2022-04-28 20:37:39 +0300

    refactor(homepage): Even more types :)

--- src/api/standings.ts
+++ src/api/standings.ts
@@ -1,10 +1,85 @@
+import { Team } from "./teams"
+
 async function RegularSeason() {
   return fetch("https://statsapi.web.nhl.com/api/v1/standings/regularSeason", {
     method: "GET",
     headers: {},
   })
     .then((response) => response.json())
+    .then((response) => response.records)
     .catch((err) => console.error(err))
+}
+
+export interface League {
+  id: number
+  name: string
+  link: string
+}
+
+export interface Division {
+  id: number
+  name: string
+  nameShort: string
+  link: string
+  abbreviation: string
+}
+
+export interface Conference {
+  id: number
+  name: string
+  link: string
+}
+
+export interface LeagueRecord {
+  wins: number
+  losses: number
+  ot: number
+  type: string
+}
+
+export interface Streak {
+  streakType: string
+  streakNumber: number
+  streakCode: string
+}
+
+export interface TeamRecord {
+  team: Team
+  leagueRecord: LeagueRecord
+  regulationWins: number
+  goalsAgainst: number
+  goalsScored: number
+  points: number
+  divisionRank: string
+  divisionL10Rank: string
+  divisionRoadRank: string
+  divisionHomeRank: string
+  conferenceRank: string
+  conferenceL10Rank: string
+  conferenceRoadRank: string
+  conferenceHomeRank: string
+  leagueRank: string
+  leagueL10Rank: string
+  leagueRoadRank: string
+  leagueHomeRank: string
+  wildCardRank: string
+  row: number
+  gamesPlayed: number
+  streak: Streak
+  clinchIndicator: string
+  pointsPercentage: number
+  ppDivisionRank: string
+  ppConferenceRank: string
+  ppLeagueRank: string
+  lastUpdated: Date
+}
+
+export interface Record {
+  standingsType: string
+  league: League
+  division: Division
+  conference: Conference
+  teamRecords: TeamRecord[]
 }
 
 export { RegularSeason }
--- src/routes/homepage.tsx
+++ src/routes/homepage.tsx
@@ -1,17 +1,20 @@
 import { useQuery } from "react-query"
-import { RegularSeason } from "../api/standings"
+import { RegularSeason, Record, TeamRecord } from "../api/standings"
 import { Link } from "react-router-dom"
 
 export default function Homepage() {
-  const query = useQuery("standings", RegularSeason)
+  const query = useQuery<Record[], Error>("standings", RegularSeason)
 
   if (query.isLoading) {
     return <div> Loading... </div>
   }
+  if (!query.isSuccess) {
+    return <div>We had an whoopsie</div>
+  }
 
   return (
     <div className="p-10 grid grid-cols-4 gap-4">
-      {query.data.records.map((record: any) => (
+      {query.data.map((record: Record) => (
         <table key={record.division.id} className="border-solid">
           <thead className="bg-gray-800">
             <tr>
@@ -20,7 +23,7 @@ export default function Homepage() {
             </tr>
           </thead>
           <tbody className="bg-gray-700">
-            {record.teamRecords.map((team: any) => (
+            {record.teamRecords.map((team: TeamRecord) => (
               <tr key={team.team.id}>
                 <Link to={`/teams/${team.team.id}`}>
                   <td className="p-2">{team.team.name}</td>
